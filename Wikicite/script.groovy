7525d59cfdc8666d2b3c5ea6a40dceddd75a9cf6
Egon Willighagen <egon.willighagen@gmail.com>
2018-03-04 19:13:05 +0100

    Script that finds articles with a particular term in the title (see https://gist.github.com/egonw/ca4c348b9a2d1116efcdb55fa85dd158/2ffded0a7bec28a6800fac9b5aee8e31504df50e)

---
+++ Wikicite/script.groovy
@@ -0,0 +1,58 @@
+// Copyright (C) 2018  Egon Willighagen
+// License: MIT
+
+totalArticleCount = 15000000
+
+batchSize = 1000000
+concept = "proteochemometric"
+conceptQ = "Q50327360"
+
+def renewFile(file) {
+  if (ui.fileExists(file)) ui.remove(file)
+  ui.newFile(file)
+  return file
+}
+
+qsFile = "/Wikicite/output.quickstatements"
+renewFile(qsFile)
+
+rounds = (int)Math.ceil(totalArticleCount / batchSize) 
+1.upto(rounds) { counter ->
+  print "batch ${counter}/${rounds}: "
+  offset = (counter-1)*batchSize
+  sparql = """
+    SELECT ?art ?artLabel
+    WITH {
+      SELECT ?art WHERE {
+        ?art wdt:P31 wd:Q13442814  
+      } LIMIT $batchSize OFFSET $offset
+    } AS %RESULTS {
+      INCLUDE %RESULTS
+      ?art wdt:P1476 ?artLabel .
+      MINUS { ?art wdt:P921 wd:$conceptQ }
+      FILTER (contains(str(?artLabel), "$concept"))
+    }
+  """
+  if (bioclipse.isOnline()) {
+    rawResults = bioclipse.sparqlRemote(
+      "https://query.wikidata.org/sparql", sparql
+    )
+    results = rdf.processSPARQLXML(rawResults, sparql)
+    missing = results.rowCount == 0
+    if (!missing) {
+      println "found ${results.rowCount} article(s)!"
+      1.upto(results.rowCount) { artCounter ->
+        artIRI = results.get(artCounter, "art")
+        artQ = artIRI.substring(31)
+        println "${artQ}\t" + results.get(artCounter, "artLabel")
+        statement = "${artQ}\tP921\t${conceptQ}\n"
+        ui.append(qsFile, statement)
+      }
+    } else {
+      println "no hits"
+    }
+  } else {
+    println "no online access"
+  }
+}
+ui.open(qsFile)
