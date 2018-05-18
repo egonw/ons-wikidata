ab5882daba50e9c10c9d9d0171dfac0058996d60
Egon Willighagen <egon.willighagen@gmail.com>
2018-05-18 20:16:03 +0200

    More stable: lcasing in search; accepts some connection fails (see https://gist.github.com/egonw/ca4c348b9a2d1116efcdb55fa85dd158/f461608b0cfde245278eff5d2e8575a139b8ce0d)

--- Wikicite/script.groovy
+++ Wikicite/script.groovy
@@ -1,11 +1,11 @@
 // Copyright (C) 2018  Egon Willighagen
 // License: MIT
 
-totalArticleCount = 15000000
+totalArticleCount = 15500000
 
-batchSize = 1000000
-concept = "proteochemometric"
-conceptQ = "Q50327360"
+batchSize = 500000
+concept = "MetaboLights"
+conceptQ = "Q24174701"
 
 def renewFile(file) {
   if (ui.fileExists(file)) ui.remove(file)
@@ -30,26 +30,30 @@ rounds = (int)Math.ceil(totalArticleCount / batchSize)
       INCLUDE %RESULTS
       ?art wdt:P1476 ?artLabel .
       MINUS { ?art wdt:P921 wd:$conceptQ }
-      FILTER (contains(str(?artLabel), "$concept"))
+      FILTER (contains(lcase(str(?artLabel)), "$concept"))
     }
   """
   if (bioclipse.isOnline()) {
     rawResults = bioclipse.sparqlRemote(
       "https://query.wikidata.org/sparql", sparql
     )
-    results = rdf.processSPARQLXML(rawResults, sparql)
-    missing = results.rowCount == 0
-    if (!missing) {
-      println "found ${results.rowCount} article(s)!"
-      1.upto(results.rowCount) { artCounter ->
-        artIRI = results.get(artCounter, "art")
-        artQ = artIRI.substring(31)
-        println "${artQ}\t" + results.get(artCounter, "artLabel")
-        statement = "${artQ}\tP921\t${conceptQ}\n"
-        ui.append(qsFile, statement)
+    try {
+      results = rdf.processSPARQLXML(rawResults, sparql)
+      missing = results.rowCount == 0
+      if (!missing) {
+        println "found ${results.rowCount} article(s)!"
+        1.upto(results.rowCount) { artCounter ->
+          artIRI = results.get(artCounter, "art")
+          artQ = artIRI.substring(31)
+          println "${artQ}\t" + results.get(artCounter, "artLabel")
+          statement = "${artQ}\tP921\t${conceptQ}\n"
+          ui.append(qsFile, statement)
+        }
+      } else {
+        println "no hits"
       }
-    } else {
-      println "no hits"
+    } catch (Exception exception) {
+      println "Error while retrieving this batch: " + e.message
     }
   } else {
     println "no online access"
