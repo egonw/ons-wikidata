fb4f074aa07371fd8a062765b42fc427e861b0cd
Egon Willighagen <egon.willighagen@gmail.com>
2018-07-30 08:39:01 +0200

    Added a script to pull in a class of lipids

---
+++ LipidMaps/endocannabinoids.groovy
@@ -0,0 +1,83 @@
+
+restAPI = "http://www.lipidmaps.org/rest/compound/lm_id/LMSP02/all/download"
+lmClass = "ceramide"
+lmClassQ = "Q424213"
+propID = "P2063"
+
+allData = bioclipse.downloadAsFile(
+  restAPI, "/LipidMaps/${lmClass}.txt"
+)
+
+
+sparql = """
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX wd: <http://www.wikidata.org/entity/>
+SELECT (substr(str(?compound),32) as ?wd) ?key ?lmid WHERE {
+  ?compound wdt:P235 ?key ; wdt:${propID} ?lmid .
+  MINUS { ?compound wdt:P31 wd:Q55282178 . }
+}
+"""
+
+if (bioclipse.isOnline()) {
+  results = rdf.sparqlRemote(
+    "https://query.wikidata.org/sparql", sparql
+  )
+}
+
+def renewFile(file) {
+  if (ui.fileExists(file)) ui.remove(file)
+  ui.newFile(file)
+  return file
+}
+
+mappingsFile = "/LipidMaps/${lmClass}Mappings.txt"
+missingCompoundFile = "/LipidMaps/${lmClass}Missing.txt"
+
+// ignore certain Wikidata items, where I don't want the LIPID MAPS ID added
+ignores = new java.util.HashSet();
+// ignores.add("Q37111097")
+
+// make a map
+map = new HashMap()
+for (i=1;i<=results.rowCount;i++) {
+  rowVals = results.getRow(i)
+  map.put(rowVals[1], rowVals[0])  
+}
+
+inchikey = ""
+batchSize = 500
+batchCounter = 0
+mappingContent = ""
+missingContent = ""
+print "Saved a batch"
+renewFile(mappingsFile)
+renewFile(missingCompoundFile)
+new File(bioclipse.fullPath("/LipidMaps/${lmClass}.txt")).eachLine{ line ->
+  fields = line.split("\t")
+  if (fields.length > 15) {
+    lmid = fields[1]
+    inchikey = fields[15]
+    if (inchikey != null && inchikey.length() > 10) {
+      batchCounter++
+      if (map.containsKey(inchikey)) {
+        wdid = map.get(inchikey)
+        if (!ignores.contains(wdid)) {
+          mappingContent += "${wdid}\tP31\t${lmClassQ}\tS143\tQ20968889\tS854\t\"${restAPI}\"\tS813\t+2018-08-16T00:00:00Z/11\n"
+        }  
+      } else {
+        missingContent += "${inchikey}\n"
+      }
+    }
+  }
+  if (batchCounter >= batchSize) {
+    ui.append(mappingsFile, mappingContent)
+    ui.append(missingCompoundFile, missingContent)
+    batchCounter = 0
+    mappingContent = ""
+    missingContent = ""
+    print "."
+  }
+}
+ui.append(mappingsFile, mappingContent)
+ui.append(missingCompoundFile, missingContent)
+println "\n"
