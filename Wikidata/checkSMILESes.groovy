8a3bb6bec96f990c157a3be0bb1079c3df4ee462
Egon Willighagen <egon.willighagen@gmail.com>
2019-04-20 15:07:36 +0200

    Use the devel version

--- Wikidata/checkSMILESes.groovy
+++ Wikidata/checkSMILESes.groovy
@@ -1,6 +1,6 @@
-@Grab(group='net.bioclipse.bacting', module='managers-cdk', version='0.0.3')
-@Grab(group='net.bioclipse.bacting', module='managers-ui', version='0.0.3')
-@Grab(group='net.bioclipse.bacting', module='managers-rdf', version='0.0.3')
+@Grab(group='net.bioclipse.bacting', module='managers-cdk', version='0.0.4-SNAPSHOT')
+@Grab(group='net.bioclipse.bacting', module='managers-ui', version='0.0.4-SNAPSHOT')
+@Grab(group='net.bioclipse.bacting', module='managers-rdf', version='0.0.4-SNAPSHOT')
 
 workspaceRoot = ".."
 ui = new net.bioclipse.managers.UIManager(workspaceRoot);
