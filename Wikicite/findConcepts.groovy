8d87cf0876a10ac470142e81df2f83a0b8232092
Egon Willighagen <egon.willighagen@gmail.com>
2019-08-15 12:59:21 +0200

    Newer versions, using CDK 2.3

--- Wikicite/findConcepts.groovy
+++ Wikicite/findConcepts.groovy
@@ -15,6 +15,8 @@
 // Changelog:
 //
 // 2018-09-01 First upload to MyExperiment.org
+@Grab(group='io.github.egonw.bacting', module='bioclipse-cdkbusiness', version='2.8.0.2')
+@Grab(group='io.github.egonw.bacting', module='bioclipse-core', version='2.8.0.2')
 @Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.5')
 @Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.5')
 @Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.5')
--- Wikidata/createWDitemsFromSMILES.groovy
+++ Wikidata/createWDitemsFromSMILES.groovy
@@ -20,11 +20,11 @@
 // 2018-12-01 Added a feature to set a superclass
 
 // Bacting config
-@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.7')
+@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.8')
 @Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.7')
 @Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.7')
-@Grab(group='io.github.egonw.bacting', module='managers-pubchem', version='0.0.7')
-@Grab(group='io.github.egonw.bacting', module='managers-inchi', version='0.0.7')
+@Grab(group='io.github.egonw.bacting', module='managers-pubchem', version='0.0.8')
+@Grab(group='io.github.egonw.bacting', module='managers-inchi', version='0.0.8')
 workspaceRoot = ".."
 ui = new net.bioclipse.managers.UIManager(workspaceRoot);
 cdk = new net.bioclipse.managers.CDKManager(workspaceRoot);
