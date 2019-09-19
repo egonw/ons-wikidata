1dab3cd2348880356e7e89b40aa7971e27847115
Egon Willighagen <egon.willighagen@gmail.com>
2019-09-19 11:34:02 +0200

    Rewritten to split output, depending on external identifiers present

--- WormMetabolites/extractChemicals.groovy
+++ WormMetabolites/extractChemicals.groovy
@@ -1,11 +1,12 @@
 @Grab(group='io.github.egonw.bacting', module='managers-excel', version='0.0.9-SNAPSHOT')
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.8')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.9-SNAPSHOT')
 
 workspaceRoot = ".."
 println "Initializing the managers..."
 
 bioclipse = new net.bioclipse.managers.BioclipseManager(workspaceRoot);
 excel = new net.bioclipse.managers.ExcelManager(workspaceRoot);
+ui = new net.bioclipse.managers.UIManager(workspaceRoot);
 
 sheet = "/WormMetabolites/SI_Table_1.xlsx"
 
@@ -13,11 +14,16 @@ println "Loading $sheet ..."
 data = excel.getSheet(sheet, 0)
 println data.getRow(2)
 
+keggOutput = "/WormMetabolites/kegg.smi"
+ui.renewFile(keggOutput)
+
 for (row in 3..3740) {
   name = data.get(row,10)
   kegg = data.get(row,12)
   smiles = data.get(row,16)
-  if (name && kegg && smiles) {
-    println "${smiles}\t${kegg}\t${name}"
+  if (name && smiles) {
+    if (kegg) {
+      ui.append(keggOutput, "${smiles}\t${kegg}\t${name}\n")
+    }
   }
 }
