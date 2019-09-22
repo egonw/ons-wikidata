73c680efacde12f315ac5a5a74a0a3925eb1039a
Egon Willighagen <egon.willighagen@gmail.com>
2019-09-22 13:57:51 +0200

    Output CSV; remove QID duplicates; check SMILES

--- PubChem/createSDF.groovy
+++ PubChem/createSDF.groovy
@@ -2,9 +2,9 @@
 // License: MIT
 
 @Grab(group='io.github.egonw.bacting', module='bioclipse-core', version='2.8.0.2')
-@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.9-SNAPSHOT')
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.8')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.9-SNAPSHOT')
 @Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.8')
+@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.8')
 
 workspaceRoot = ".."
 ui = new net.bioclipse.managers.UIManager(workspaceRoot);
@@ -12,22 +12,31 @@ rdf = new net.bioclipse.managers.RDFManager(workspaceRoot);
 bioclipse = new net.bioclipse.managers.BioclipseManager(workspaceRoot);
 cdk = new net.bioclipse.managers.CDKManager(workspaceRoot);
 
+output = "/PubChem/wikidata.csv"
+ui.renewFile(output)
+
 // the following query gets all chemical with a canonical or isomeric SMILES
 sparql = """
 SELECT (STR(?chemicalRes) as ?chemical) ?chemicalResLabel ?canonical ?isomeric WITH {
   SELECT DISTINCT ?chemicalRes WHERE {
     ?chemicalRes wdt:P233 | wdt:P2017 [] .
-  } LIMIT 5
+  } LIMIT 1000
 } AS %CHEMICALS { 
   INCLUDE %CHEMICALS
   OPTIONAL { ?chemicalRes wdt:P233 ?canonical }
   OPTIONAL { ?chemicalRes wdt:P2017 ?isomeric }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-} 
+}
   """
 
-output = "/PubChem/wikidata.sdf"
+regIDs = new HashSet<String>();
 
+ui.append(output,
+  "PUBCHEM_EXT_DATASOURCE_REGID," +
+  "PUBCHEM_SUBSTANCE_SYNONYM," +
+  "PUBCHEM_EXT_SUBSTANCE_URL," +
+  "PUBCHEM_EXT_DATASOURCE_SMILES\n"
+)
 if (bioclipse.isOnline()) {
   try {
     rawResults = bioclipse.sparqlRemote(
@@ -39,14 +48,26 @@ if (bioclipse.isOnline()) {
       label  = results.get(chemCounter, "chemicalResLabel")
       canSmi = results.get(chemCounter, "canonical")
       isoSmi = results.get(chemCounter, "isomeric")
-      mol = (isoSmi ? cdk.fromSMILES(isoSmi) : cdk.fromSMILES(canSmi))
-      mol.atomContainer.title = wdid
+      outSmi = (isoSmi ? isoSmi : canSmi)
       qid = wdid.substring(31)
-      mol.setProperty("PUBCHEM_EXT_DATASOURCE_REGID", qid)
-      mol.setProperty("Label0", label)
-      mol.setProperty("URL0", "https://tools.wmflabs.org/scholia/" + qid)
-      mol.setProperty("URL0_TITLE", label)
-      cdk.appendToSDF(output, mol)
+      if (!regIDs.contains(qid)) {
+        regIDs.add(qid)
+        try {
+          mol = cdk.fromSMILES(outSmi)
+          outCmp = "${qid},"
+          label = label.replaceAll("\"", "''").replaceAll('\n',"").replaceAll('\r',"")
+          if (label.contains(",")) {
+            outCmp += "\"" + label + "\","
+          } else {
+            outCmp += label + ","
+          }
+          outCmp += "https://tools.wmflabs.org/scholia/${qid},"
+          outCmp += "$outSmi\n"
+          ui.append(output, outCmp)
+        } catch (Exception exception) {
+          println("${qid}: ${exception.message}")
+        }
+      }
     }
   } catch (Exception exception) {
     println exception.message
