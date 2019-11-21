e61f184631aa8cd938e7d96afdf88c1bdf521d03
Egon Willighagen <egon.willighagen@gmail.com>
2019-11-21 21:12:23 +0100

    Updated deps

--- PubChem/createSDF.groovy
+++ PubChem/createSDF.groovy
@@ -1,10 +1,9 @@
 // Copyright (C) 2019  Egon Willighagen
 // License: MIT
 
-@Grab(group='io.github.egonw.bacting', module='bioclipse-core', version='2.8.0.2')
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.9-SNAPSHOT')
-@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.8')
-@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.8')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.9')
+@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.9')
+@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.9')
 
 workspaceRoot = ".."
 ui = new net.bioclipse.managers.UIManager(workspaceRoot);
