9704c57cd3c5182a872b4e159e300cccb02080c6
Egon Willighagen <egon.willighagen@gmail.com>
2021-09-12 07:48:47 +0200

    Removed debug info

--- OpenCitations/quickstatements2.groovy
+++ OpenCitations/quickstatements2.groovy
@@ -59,7 +59,6 @@ citedDOIs.each { doi ->
 
 // find QIDs for articles citing the focus article, but not if they already cite it in Wikidata (MINUS clause)
 sparql = "SELECT DISTINCT ?work ?doi WHERE {\n VALUES ?doi {\n ${values} }\n ?work wdt:P356 ?doi . MINUS { ?citingWork wdt:P356 \"${doi}\" ; wdt:P2860 ?work }\n}"
-println sparql
 
 if (bioclipse.isOnline()) {
   rawResults = bioclipse.sparqlRemote(
@@ -76,8 +75,6 @@ for (i=1;i<=results.rowCount;i++) {
 }
 
 citingQID = map.get(doi)
-
-println "" + map
 
 String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
 
