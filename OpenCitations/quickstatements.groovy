8be4c4280476b34b1bc4ab0fc0dab6a83f1eaefb
Egon Willighagen <egon.willighagen@gmail.com>
2021-10-02 14:24:34 +0200

    Merged to two tools, as the QS output has the same columns anyway

--- OpenCitations/quickstatements.groovy
+++ OpenCitations/quickstatements.groovy
@@ -43,18 +43,23 @@ if (options.doi) {
   System.exit(0)
 }
 
+String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
 cociURL = new URL("https://opencitations.net/index/coci/api/v1/citations/${doi}")
 println "Fetching ${doi} from ${cociURL}..."
-
 data = new groovy.json.JsonSlurper().parseText(cociURL.text)
-
 citingDOIs = new java.util.HashSet()
+data.each { citation -> citingDOIs.add(citation.citing) }
+println "Found citing DOIs: ${citingDOIs.size()}"
 
-data.each { citation ->
-  citingDOIs.add(citation.citing)
-}
+cociURL = new URL("https://opencitations.net/index/coci/api/v1/references/${doi}")
+println "Fetching ${doi} from ${cociURL}..."
+data2 = new groovy.json.JsonSlurper().parseText(cociURL.text)
+citedDOIs = new java.util.HashSet()
+data2.each { citation -> citedDOIs.add(citation.cited) }
+println "Found cited DOIs: ${citedDOIs.size()}"
 
-println "Found DOI: ${citingDOIs.size()}"
+// citing papers
 
 values = "\"${doi}\" \n" // we also need a QID for the cited article
 citingDOIs.each { doi ->
@@ -63,7 +68,34 @@ citingDOIs.each { doi ->
 
 // find QIDs for articles citing the focus article, but not if they already cite it in Wikidata (MINUS clause)
 sparql = "SELECT DISTINCT ?work ?doi WHERE {\n VALUES ?doi {\n ${values} }\n ?work wdt:P356 ?doi . MINUS { ?work wdt:P2860/wdt:P356 \"${doi}\" }\n}"
+if (bioclipse.isOnline()) {
+  rawResults = bioclipse.sparqlRemote("https://query.wikidata.org/sparql", sparql  )
+  results = rdf.processSPARQLXML(rawResults, sparql)
+}
 
+// make a map
+map = new HashMap<String,String>()
+for (i=1;i<=results.rowCount;i++) {
+  rowVals = results.getRow(i)
+  map.put(rowVals[1], rowVals[0].replace("http://www.wikidata.org/entity/",""))
+}
+
+citedQID = map.get(doi)
+println "qid,P2860,S248,s854,s813"
+println "# citing articles"
+map.each { citingDOI, qid ->
+  if (citingDOI != doi) println "${qid},${citedQID},Q107507940,\"\"\"${cociURL}\"\"\",+${date}T00:00:00Z/11"
+}
+
+// cited papers
+
+values = "\"${doi}\" \n" // we also need a QID for the citing article
+citedDOIs.each { doi ->
+  values += "\"${doi.toUpperCase()}\" \n"
+}
+
+// find QIDs for articles citing the focus article, but not if they already cite it in Wikidata (MINUS clause)
+sparql = "SELECT DISTINCT ?work ?doi WHERE {\n VALUES ?doi {\n ${values} }\n ?work wdt:P356 ?doi . MINUS { ?citingWork wdt:P356 \"${doi}\" ; wdt:P2860 ?work }\n}"
 if (bioclipse.isOnline()) {
   rawResults = bioclipse.sparqlRemote(
     "https://query.wikidata.org/sparql", sparql
@@ -78,14 +110,9 @@ for (i=1;i<=results.rowCount;i++) {
   map.put(rowVals[1], rowVals[0].replace("http://www.wikidata.org/entity/",""))
 }
 
-citedQID = map.get(doi)
-
-// println "" + map
-
-String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-
-println "qid,P2860,S248,s854,s813"
-map.each { citingDOI, qid ->
-  if (citingDOI != doi) println "${qid},${citedQID},Q107507940,\"\"\"${cociURL}\"\"\",+${date}T00:00:00Z/11"
+citingQID = map.get(doi)
+println "# cited articles"
+map.each { citedDOI, qid ->
+  if (citedDOI != doi) println "${citingQID},${qid},Q107507940,\"\"\"${cociURL}\"\"\",+${date}T00:00:00Z/11"
 }
 
