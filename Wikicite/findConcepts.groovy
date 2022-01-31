e9deb4beb371a262cdbb0c95751f5d0116fdd400
Egon Willighagen <egon.willighagen@gmail.com>
2022-01-31 20:25:22 +0100

    Updated to Bacting 0.0.31

--- Wikicite/findConcepts.groovy
+++ Wikicite/findConcepts.groovy
@@ -15,9 +15,9 @@
 // Changelog:
 //
 // 2018-09-01 First upload to MyExperiment.org
-@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.10')
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.10')
-@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.10')
+@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.0.31')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.31')
+@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.31')
 
 workspaceRoot = ".."
 ui = new net.bioclipse.managers.UIManager(workspaceRoot);
