2c11c6566255eacf8defb6e393ee0e7fb97f94fd
Egon Willighagen <egon.willighagen@gmail.com>
2022-06-06 16:55:32 +0200

    Do all npm package already in Wikidata

--- ScholiaUse/npmDeps.groovy
+++ ScholiaUse/npmDeps.groovy
@@ -13,51 +13,66 @@ rdf = new net.bioclipse.managers.RDFManager(".")
 
 def parser = new JsonSlurper()
 
-npm = "citation-js"
+// single package
+npmPkgs = [ "citation-js" ]
 
-// STEP 0: figure out what package this Wikidata item is for
-
-sparql = """
-PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-SELECT ?qid WHERE {
-  ?qid wdt:P8262 \"$npm\" .
-}"""
-
-results = rdf.sparqlRemote("https://query.wikidata.org/sparql", sparql)
-qid = results.get(1, "qid")
-// println("package: $npm ($qid)")
-
-jsonText = bioclipse.download("https://registry.npmjs.com/$npm")
-//println(jsonText)
-
-def jsonResp = parser.parseText(jsonText)
-version = jsonResp."dist-tags".latest
-
-
-// DEPENDENCIES
-deps = jsonResp.versions."$version".dependencies
-depsList = deps.keySet()
-depListVals = ""
-depsList.each { dep ->
-  // println("dependency: $dep")
-  depListVals += "\"$dep\" ";
-}
+// OR all packages in Wikidata
 
 sparql = """
 PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-SELECT ?package ?npm WHERE {
-  VALUES ?npm { $depListVals}
-  ?package wdt:P8262 ?npm .
-  MINUS { $qid wdt:P1547 ?package }
+SELECT DISTINCT ?npm WHERE {
+  ?qid wdt:P8262 ?npm .
 }"""
 results = rdf.sparqlRemote("https://query.wikidata.org/sparql", sparql)
+npmPkgs = results.getColumn("npm")
 
 println "qid,P1547"
-for (i=1;i<=results.rowCount;i++) {
-  rowVals = results.getRow(i)
-  qid = qid.replace("wd:","")
-  pkg = rowVals[0].replace("wd:","")
-  println("$qid,$pkg")
+
+npmPkgs.each { npm ->
+  // STEP 0: figure out what package this Wikidata item is for
+
+  sparql = """
+  PREFIX wd: <http://www.wikidata.org/entity/>
+  PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+  SELECT ?qid WHERE {
+    ?qid wdt:P8262 \"$npm\" .
+  }"""
+
+  results = rdf.sparqlRemote("https://query.wikidata.org/sparql", sparql)
+  qid = results.get(1, "qid")
+  println("# package: $npm ($qid)")
+
+  jsonText = bioclipse.download("https://registry.npmjs.com/$npm")
+  //println(jsonText)
+
+  def jsonResp = parser.parseText(jsonText)
+  version = jsonResp."dist-tags".latest
+
+  // Step 1: get the DEPENDENCIES
+  deps = jsonResp.versions."$version".dependencies
+  if (deps) {
+    depListVals = ""
+    deps.each { dep, version ->
+      // println("dependency: $dep")
+      if (!version.contains("/")) depListVals += "\"$dep\" ";
+    }
+
+    sparql = """
+    PREFIX wd: <http://www.wikidata.org/entity/>
+    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+    SELECT ?package ?npm WHERE {
+      VALUES ?npm { $depListVals}
+      ?package wdt:P8262 ?npm .
+      MINUS { $qid wdt:P1547 ?package }
+    }"""
+    results = rdf.sparqlRemote("https://query.wikidata.org/sparql", sparql)
+
+    for (i=1;i<=results.rowCount;i++) {
+      rowVals = results.getRow(i)
+      qid = qid.replace("wd:","")
+      pkg = rowVals[0].replace("wd:","")
+      println("$qid,$pkg")
+    }
+  }
 }
