9a8c5b2c7924bb83389017fdf89f4ed6357ab2fe
Egon Willighagen <egon.willighagen@gmail.com>
2023-07-21 13:17:59 +0200

    Replaced the no longer needed 'offset' with option to exclude articles that have one or more other 'main subjects' (e.g. not tag COVID-19 if 'COVID-19 vaccine' is already tagged)

--- Wikicite/findConcepts.groovy
+++ Wikicite/findConcepts.groovy
@@ -15,9 +15,9 @@
 // Changelog:
 //
 // 2018-09-01 First upload to MyExperiment.org
-@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.1.2')
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.1.2')
-@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.1.2')
+@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.3.2')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.3.2')
+@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.3.2')
 
 import groovy.cli.commons.CliBuilder
 
@@ -37,7 +37,7 @@ def cli = new CliBuilder(usage: 'findConcepts.groovy')
 cli.h(longOpt: 'help', 'print this message')
 cli.s(longOpt: 'search-string', args:1, argName:'query', 'Search this query in the article titles')
 cli.q(longOpt: 'qid', args:1, argName:'qid', 'QID of the item to set as main item')
-cli.o(longOpt: 'offset', args:1, argName:'offset', 'Number of batches (batch size: ${batchSize}) to skip')
+cli.x(longOpt: 'x', args:1, argName:'exclude', 'Comma-separated list of QIDs that must not be main subject (either)')
 def options = cli.parse(args)
 
 if (options.help) {
@@ -47,10 +47,19 @@ if (options.help) {
 
 concept = null
 conceptQ = null
+excludeTopics = new HashSet<String>()
 if (options.s) concept = options.s
 if (options.q) conceptQ = options.q
 startBatch = 0
-if (options.o) startBatch = Integer.parseInt(options.o)
+if (options.x) {
+  if (options.x.contains(",")) {
+    for (topic in options.x.split(',')) {
+      excludeTopics.add(topic)
+    }
+  } else {
+    excludeTopics.add(options.x)
+  }
+}
 
 if (concept == null | conceptQ == null) {
   println "A search string and QID must be given."
@@ -67,11 +76,6 @@ blacklist = [
 originalConcept = concept
 concept = concept.toLowerCase()
 
-totalArticleCount = 30000000
-batchSize = totalArticleCount
-
-start = startBatch*batchSize
-
 def renewFile(file) {
   if (ui.fileExists(file)) ui.remove(file)
   ui.newFile(file)
@@ -82,25 +86,29 @@ qsFile = "/Wikicite/output." + concept.replace(" ", "_") + ".quickstatements"
 renewFile(qsFile)
 ui.append(qsFile, "qid,P921,S887,s1932,s813,#\n")
 
-rounds = (int)Math.ceil((totalArticleCount-start) / batchSize)
-1.upto(rounds) { counter ->
-  print "batch ${counter}/${rounds}: "
-  offset = start + (counter-1)*batchSize
-  // query based on example by Lucas Werkmeister in the Wikidata Telegram group
-  sparql = """
-    SELECT ?art ?artTitle
-    WHERE {
-      SERVICE wikibase:mwapi {
-        bd:serviceParam wikibase:endpoint "www.wikidata.org";
-          wikibase:api "Search";
-          mwapi:srsearch "$concept haswbstatement:P31=Q13442814 -haswbstatement:P921=$conceptQ";
-          mwapi:srlimit "max".
-        ?art wikibase:apiOutputItem mwapi:title.
-      }
-      ?art wdt:P1476 ?artTitle.
-      FILTER (contains(lcase(str(?artTitle)), "$concept"))
+excludeSPARQL = ""
+if (excludeTopics.size() > 0) {
+  excludeQIDs = ""
+  for (qid in excludeTopics) excludeQIDs += "wd:${qid} "
+  excludeSPARQL = "VALUES ?excludeQID { ${excludeQIDs}} MINUS { ?art wdt:P921 ?excludeQID }"
+}
+
+sparql = """
+  SELECT DISTINCT ?art ?artTitle
+  WHERE {
+    ${excludeSPARQL}
+    SERVICE wikibase:mwapi {
+      bd:serviceParam wikibase:endpoint "www.wikidata.org";
+        wikibase:api "Search";
+        mwapi:srsearch "$concept haswbstatement:P31=Q13442814 -haswbstatement:P921=$conceptQ";
+        mwapi:srlimit "max".
+      ?art wikibase:apiOutputItem mwapi:title.
     }
-  """
+    ?art wdt:P1476 ?artTitle.
+    FILTER (contains(lcase(str(?artTitle)), "$concept"))
+  }
+"""
+
   if (bioclipse.isOnline()) {
     try {
       // print sparql
@@ -142,5 +150,4 @@ rounds = (int)Math.ceil((totalArticleCount-start) / batchSize)
   } else {
     println "no online access"
   }
-}
 //ui.open(qsFile)
