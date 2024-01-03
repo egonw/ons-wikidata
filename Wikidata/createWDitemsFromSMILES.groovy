48ea5d184872a257ca0bcec9daed2c167cd06d86
Egon Willighagen <egon.willighagen@gmail.com>
2024-01-03 14:08:45 +0100

    Use a newer Bacting

--- Wikidata/createWDitemsFromSMILES.groovy
+++ Wikidata/createWDitemsFromSMILES.groovy
@@ -15,11 +15,11 @@
 //     http://quickstatements.toolforge.org/
 
 // Bacting config
-@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.4.0')
-@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.4.0')
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.4.0')
-@Grab(group='io.github.egonw.bacting', module='managers-pubchem', version='0.4.0')
-@Grab(group='io.github.egonw.bacting', module='managers-inchi', version='0.4.0')
+@Grab(group='io.github.egonw.bacting', module='managers-cdk', version='0.5.0')
+@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.5.0')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.5.0')
+@Grab(group='io.github.egonw.bacting', module='managers-pubchem', version='0.5.0')
+@Grab(group='io.github.egonw.bacting', module='managers-inchi', version='0.5.0')
 
 import groovy.cli.commons.CliBuilder
 
